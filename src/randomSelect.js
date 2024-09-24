d4968a962b49ca967db4d4b8abdc50afd4d5a63a
Stefan Holmberg <stefan.holmberg@systementor.se>
2024-09-24 14:45:26 +0200

    Update randomSelect.js

--- src/randomSelect.js
+++ src/randomSelect.js
@@ -1,5 +1,4 @@
 function randomFromArray(arr){
-    //Your code here to select a random element from the array passed to the function.
+    //TODO: Your code here to select a random element from the array passed to the function.
   }
-  
-  //TODO: Export the randomFromArray function.
+    //TODO: Export the randomFromArray with name randomSelect function.
